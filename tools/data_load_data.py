b5c51306078e4de6f900bf79c6af849fb39d63e7
rafaelxiao <rafaelxiao@hotmail.com>
2019-06-24 15:35:17 +0800

    finished end_to_end_project

--- tools/data_load_data.py
+++ tools/data_load_data.py
@@ -1,8 +1,11 @@
 import os
+import pandas as pd
 import scipy.io
 
 def load_data(name):
-    path = os.path.abspath(os.path.join(os.getcwd(), "..%s..%spmtk3data"%(os.sep, os.sep)))
+    path = os.path.abspath(os.path.join(os.getcwd(), "..%s..%sdatasets"%(os.sep, os.sep)))
     forma = name.split('.')[-1]
     if forma == 'mat':
         return scipy.io.loadmat(os.path.join(path, name))
+    if forma == 'csv':
+        return pd.read_csv(os.path.join(path, name))
